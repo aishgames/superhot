8d1fb547114c013a3339dae4df339d62ba8d3a53
makra <58925008+aniketrajnish@users.noreply.github.com>
2021-03-13 11:40:21 +0530

    Create SuperHot.cs

---
+++ SuperHot.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SuperHot : MonoBehaviour
+{
+    bool yeet;
+    // start the couroutine ActionYeet wherever minor actions like picking and throwing objects are to be performed!
+    public IEnumerator Yeet(float time)
+    {
+        yeet = true;
+        yield return new WaitForSecondsRealtime(.06f);
+        yeet = false;
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        float x = Input.GetAxisRaw("Horizontal");
+
+        float y = Input.GetAxisRaw("Vertical");
+
+
+        float time = (x != 0 || y != 0) ? 0.02f : 1f;
+        float lerpTime = (x != 0 || y != 0) ? .05f : .5f;
+
+        time = yeet ? 1 : time;
+        lerpTime = yeet ? .1f : lerpTime;
+
+        Time.timeScale = Mathf.Lerp(Time.timeScale, time, lerpTime);
+    }
+}
